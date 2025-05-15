a2508269bdb9be3b36febcc69ea4cdad206c57b6
ISABELLA ANGELA <isant482@gmail.com>
2025-05-15 15:55:04 -0300

    Aplica .gitignore para ignorar arquivos desnecessários

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from .models import Question
 from django.shortcuts import render
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 
@@ -43,3 +45,14 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
